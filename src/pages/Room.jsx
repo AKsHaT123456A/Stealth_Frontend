4abe8cc3653a77f5bcefc61ab5c40178a3e03bf7
Akshat Mishra <104546329+AKsHaT123456A@users.noreply.github.com>
2023-09-01 17:02:43 +0530

    Update

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,33 +1,56 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
+import axios from "axios";
 
 const Room = () => {
+  const [isAccepted, setisAccepted] = useState(false);
+  const [isRejected, setisRejected] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [message, setMessage] = useState("Loading...");
   const { roomId } = useParams();
-const username = roomId;
-  const meet = async (element) => {
-    const appId = 1903949555;
-    const serverSecret = "24bced055e2cc9835f8d16411299bc18";
-    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
-      appId,
-      serverSecret,
-      roomId,
-      Date.now().toString(),
-      username 
-    );
-    const zp = ZegoUIKitPrebuilt.create(kitToken);
-const a = 0;
-    zp.joinRoom({
-      container: element,
-      scenario: {
-        mode: ZegoUIKitPrebuilt.VideoConference,
-      },
+  const username = roomId;
+
+  const meetElementRef = useRef(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/v1/video/call?roomName=Abcdef").then((res) => {
+      console.log(res.data);
+      if (res.data.isAccepted) {
+        setisAccepted(true);
+        setLoading(false);
+        setMessage("Call Accepted");
+      } else if (res.data.isRejected) {
+        setisRejected(true);
+        setMessage("Call Rejected");
+      }
     });
-  };
+  }, [isAccepted, isRejected]);
+
+  useEffect(() => {
+    if (!loading && meetElementRef.current) {
+      const appId = 1903949555;
+      const serverSecret = "24bced055e2cc9835f8d16411299bc18";
+      const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+        appId,
+        serverSecret,
+        roomId,
+        Date.now().toString(),
+        username
+      );
+      const zp = ZegoUIKitPrebuilt.create(kitToken);
+      zp.joinRoom({
+        container: meetElementRef.current,
+        scenario: {
+          mode: ZegoUIKitPrebuilt.VideoConference,
+        },
+      });
+    }
+  }, [loading, roomId, username]);
 
   return (
     <div className="room">
-      <div ref={meet} />
+      {loading ? <p>{message}</p> : <div ref={meetElementRef} />}
     </div>
   );
 };
